37a1b3e8fd23152a08b40bc994ad1dd454f0055e
Dmytro <dmytro.siroshtan@gmail.com>
2025-06-09 09:50:17 +0200

    feat: export components into index

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export * from './feed';
+export * from './form';
+export * from './nav';
+export * from './profile';
+export * from './prompt-card';
+export * from './prompt-card-list';
+export * from './provider';
