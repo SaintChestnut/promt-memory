3f648b4905a78f204ab5b17edc12c13ff1b67610
Dmytro <dmytro.siroshtan@gmail.com>
2025-06-09 09:42:41 +0200

    feat: add routes resources

---
+++ resources/routes.ts
@@ -0,0 +1,24 @@
+export type PageRoutesType = 'HOME' | 'CREATE_PROMPT' | 'EDIT_PROMPT' | 'PROFILE';
+
+export const pageRoutes: Record<PageRoutesType, string> = {
+  HOME: '/',
+  CREATE_PROMPT: '/create-prompt',
+  EDIT_PROMPT: '/edit-prompt',
+  PROFILE: '/profile'
+};
+
+export type ApiRoutesType = 'NEW_PROMPT' | 'PROMPT_BY_ID' | 'USER_POSTS' | 'ALL_PROMPTS';
+
+export const apiRoutes: Record<ApiRoutesType, string> = {
+  ALL_PROMPTS: '/api/prompt',
+  NEW_PROMPT: '/api/prompt/new',
+  PROMPT_BY_ID: '/api/prompt/[promptId]',
+  USER_POSTS: '/api/users/[userId]/posts'
+};
+
+export type SearchParamsType = 'PROMPT_ID' | 'PROMPT_TAG';
+
+export const searchParams: Record<SearchParamsType, string> = {
+  PROMPT_ID: 'promptId',
+  PROMPT_TAG: 'promptTag'
+};
