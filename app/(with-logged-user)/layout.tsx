6c61f123a7f67fc6b07e73adf2ba9fd4a1962be6
Dmytro <dmytro.siroshtan@gmail.com>
2025-06-26 14:09:27 +0200

    feat(provider): implement user provider

--- app/(with-logged-user)/layout.tsx
+++ app/(with-logged-user)/layout.tsx
@@ -1,19 +1,14 @@
-'use client';
-
-import { UserProvider } from '@/components/providers/user-provide';
+import { UserProvider } from '@/components/providers/user-provider';
 import '@styles/globals.css';
-import { useSession } from 'next-auth/react';
 
 const Layout = ({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const { data: session } = useSession();
-
   return (
     <div lang="en">
-      <UserProvider session={session}>{children}</UserProvider>
+      <UserProvider>{children}</UserProvider>
     </div>
   );
 };
