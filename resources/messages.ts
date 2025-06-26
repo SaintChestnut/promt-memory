0bb3455d3281fd43b3bc7210012fae8adfd9d703
Dmytro <dmytro.siroshtan@gmail.com>
2025-06-26 22:28:42 +0200

    feat(global messages): create messages object

---
+++ resources/messages.ts
@@ -0,0 +1,49 @@
+export const messages = {
+  credentialsForm: {
+    username: 'Username',
+    password: 'Password',
+    email: 'Email'
+  },
+  mainHeader: 'Discover and Share',
+  mainSubHeader: 'AI-Powered Prompts',
+  noUser: {
+    header: 'No logged user in session'
+  },
+
+  signIn: 'Sign In',
+  signUp: 'Sign Up',
+  createAccount: 'Create an account',
+  profile: {
+    header: 'Profile'
+  },
+  promtCard: {
+    buttons: {
+      edit: 'Edit',
+      delete: 'Delete'
+    }
+  },
+  navigationBar: {
+    logoText: 'Prompt Memory',
+    buttons: {
+      createPromt: 'Create Prompt',
+      myProfile: 'My Profile',
+      signOut: 'Sign Out'
+    }
+  },
+  modal: {
+    closeButton: 'Close Modal'
+  },
+  createForm: {
+    header: 'Post',
+    description: 'Create and share your AI prompts with the world',
+    promt: {
+      label: 'Your AI Prompt',
+      placeholder: 'Write your prompt here...'
+    },
+    tag: {
+      label: 'Tag',
+      placeholder: '#tag'
+    },
+    cancelButton: 'Cancel'
+  }
+};
